fb7a21eb72e39c3f542b18a08738f54d6c3a1de6
Test0-VC <omarabdin1@gmail.com>
2025-04-19 15:33:33 +0200

    added error message if doctor.lenght  === 0

--- src/pages/Doctors/DoctorList.jsx
+++ src/pages/Doctors/DoctorList.jsx
@@ -1,36 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Stethoscope, AlertCircle } from "lucide-react";
 import doctors from "../Doctors/MockData.js";
 
 const DoctorList = () => {
   return (
-    <div className="max-w-6xl mx-auto px-4 py-12 text-center" dir="rtl" >
+    <div className="min-h-screen max-w-6xl mx-auto px-4 py-12 text-center" dir="rtl">
       <h2 className="text-4xl font-bold text-blue-900 mb-6">فريق الأطباء</h2>
       <p className="text-gray-700 mb-10">
         في مجمع ابتسامة الورود، نفخر بفريقنا الطبي المتميز من أطباء الأسنان.
       </p>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-        {doctors.map((doctor) => (
-          <div
-            key={doctor.id}
-            className="bg-white shadow-lg rounded-lg overflow-hidden text-center"
-          >
-            <img src={doctor.image} alt={doctor.name} className="w-full object-cover" />
-            <div className="p-4 bg-blue-50">
-              <h3 className="text-blue-900 font-semibold">{doctor.name}</h3>
-              <p className="text-gray-600">{doctor.specialty}</p>
-              <p className="text-gray-500 text-sm mt-1">نبذة عن {doctor.name}</p>
-              <Link
-                to={`/doctor/${doctor.slug}`}
-                className="text-blue-600 mt-2 inline-block underline"
-              >
-                رابط الموقع الشخصي
-              </Link>
+      {doctors.length === 0 ? (
+        <div className="flex items-center justify-center min-h-[60vh]">
+       <div className="text-blue-900 text-lg mt-16 flex flex-col items-center gap-8">
+       <Stethoscope size={100} className="text-blue-900" />
+       <span className="text-xl font-semibold">لا يوجد أطباء حالياً</span>
+     </div>
+     </div>
+     
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          {doctors.map((doctor) => (
+            <div
+              key={doctor.id}
+              className="bg-white shadow-lg rounded-lg overflow-hidden text-center"
+            >
+              <img src={doctor.image} alt={doctor.name} className="w-full object-cover" />
+              <div className="p-4 bg-blue-50">
+                <h3 className="text-blue-900 font-semibold">{doctor.name}</h3>
+                <p className="text-gray-600">{doctor.specialty}</p>
+                <p className="text-gray-500 text-sm mt-1">نبذة عن {doctor.name}</p>
+                <Link
+                  to={`/doctor/${doctor.slug}`}
+                  className="text-blue-600 mt-2 inline-block underline"
+                >
+                  رابط الموقع الشخصي
+                </Link>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
