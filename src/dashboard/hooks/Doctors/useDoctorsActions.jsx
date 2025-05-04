24bf126dbad5affa1cb1c13be84b0453bbc932b2
Abdeen-coder <oanwar19999@gmail.com>
2025-05-04 19:07:04 +0300

    UseDoctor Actions for Calling the Api responsible for Delete , Edit , Update image, Get All Doctors

---
+++ src/dashboard/hooks/Doctors/useDoctorsActions.jsx
@@ -0,0 +1,139 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+const useDoctorsActions = () => {
+  const [doctors, setDoctors] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [addLoading, setAddLoading] = useState(false);
+  const [editLoading, setEditLoading] = useState(false);
+  const [deleteLoading, setDeleteLoading] = useState(false);
+  const [imageUploadLoading, setImageUploadLoading] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const BaseURL = "http://localhost:5000/api/v1/doctor";
+
+  const page = parseInt(searchParams.get("page")) || 1;
+  const size = 6;
+  const search = searchParams.get("search") || "";
+
+
+// Get All Doctors
+  const fetchDoctors = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(`${BaseURL}?page=${page}&size=${size}&search=${search}`, {
+        withCredentials: true,
+      });
+      setDoctors(response.data.doctors || []);
+      setTotal(response.data.totalDoctors || 0);
+    } catch (error) {
+      console.error("Failed to fetch doctors:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  // Add Doctor
+  const addDoctor = async (formData) => {
+    setAddLoading(true);
+    try {
+      const response = await axios.post(BaseURL, formData, {
+        withCredentials: true,
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      await fetchDoctors();
+      return { success: true, message: response.data.message };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.response?.data?.message || "خطأ أثناء الإضافة",
+      };
+    } finally {
+      setAddLoading(false);
+    }
+  };
+  // Update Doctor
+  const updateDoctor = async (id, data) => {
+    setEditLoading(true);
+    try {
+      const response = await axios.put(`${BaseURL}/${id}`, data, {
+        withCredentials: true,
+      });
+      await fetchDoctors();
+      return { success: true, message: response.data.message };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.response?.data?.message || "خطأ أثناء التعديل",
+      };
+    } finally {
+      setEditLoading(false);
+    }
+  };
+  // Update Doctor Image
+  const updateDoctorImage = async (id, formData) => {
+    setImageUploadLoading(true);
+    try {
+      const response = await axios.put(`${BaseURL}/image/${id}`, formData, {
+        withCredentials: true,
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      await fetchDoctors();
+      return { success: true, message: response.data.message };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.response?.data?.message || "فشل تحديث الصورة",
+      };
+    } finally {
+      setImageUploadLoading(false);
+    }
+  };
+   // Delete Doctor
+  const deleteDoctor = async (id) => {
+    setDeleteLoading(true);
+    try {
+      const response = await axios.delete(`${BaseURL}/${id}`, {
+        withCredentials: true,
+      });
+      await fetchDoctors();
+      return { success: true, message: response.data.message };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.response?.data?.message || "فشل في الحذف",
+      };
+    } finally {
+      setDeleteLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDoctors();
+  }, [page, size, search]);
+
+  const handlePageChange = (newPage) => {
+    setSearchParams({ page: newPage.toString(), search });
+  };
+
+  return {
+    doctors,
+    total,
+    page,
+    size,
+    totalPages: Math.ceil(total / size),
+    loading,
+    addLoading,
+    editLoading,
+    deleteLoading,
+    imageUploadLoading,
+    addDoctor,
+    updateDoctor,
+    updateDoctorImage,
+    deleteDoctor,
+    handlePageChange,
+  };
+};
+
+export default useDoctorsActions;
