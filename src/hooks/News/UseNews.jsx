bf9f6acb89a9e7d7ba4930b21763e916228c6157
Mohamed Hamed <125780715+Mohamed-Hamed-20@users.noreply.github.com>
2025-05-04 18:29:11 +0300

    another push

--- src/hooks/News/UseNews.jsx
+++ src/hooks/News/UseNews.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 
-export default function UseNews() {
+export default function UseNews(page) {
   const BASE_URL = "http://localhost:5000/api/v1/news/";
   const [data, setData] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
@@ -12,7 +12,7 @@ export default function UseNews() {
       try {
         setLoading(true);
         setError(null);
-        const res = await axios.get(`${BASE_URL}`);
+        const res = await axios.get(`${BASE_URL}?page=${page}`);
         setData(res.data.data || []);
         console.log(data);
       } catch (err) {
@@ -26,4 +26,33 @@ export default function UseNews() {
     fetchNews();
   }, []);
   return { data, loading, error };
+}
+
+export function NewById(id) {
+  const BASE_URL = "http://localhost:5000/api/v1/news";
+  const [New, setNew] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const res = await axios.get(`${BASE_URL}/${id}`);
+
+        console.log(res);
+        setNew(res.data.data || []);
+        console.log(New);
+      } catch (err) {
+        console.error(err);
+        setError("فشل في تحميل بيانات الأخبار");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNews();
+  }, []);
+  return { New, loading, error };
 }
